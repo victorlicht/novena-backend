10dddeabeeee90cb0cc2a303194992a423f503b7
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-12-02 22:48:48 +0100

    Set Doctor Model & Dto

--- src/main/java/com/victorlicht/novenabackend/dtos/DoctorDto.java
+++ src/main/java/com/victorlicht/novenabackend/dtos/DoctorDto.java
@@ -1,4 +1,17 @@
 package com.victorlicht.novenabackend.dtos;
 
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class DoctorDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date dateOfBirth;
+
+    private String phoneNumber;
 }
--- src/main/java/com/victorlicht/novenabackend/models/Doctor.java
+++ src/main/java/com/victorlicht/novenabackend/models/Doctor.java
@@ -1,4 +1,29 @@
 package com.victorlicht.novenabackend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@Entity
 public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date dateOfBirth;
+
+    private String phoneNumber;
+
 }
