b3e577ed4201137fa557f951ce2acf4b22e484f4
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-12-03 20:55:21 +0100

    Add controller for the template on general(Working fine) with method returned home.html

---
+++ src/main/java/com/victorlicht/novenabackend/controllers/TemplateController.java
@@ -0,0 +1,14 @@
+package com.victorlicht.novenabackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TemplateController {
+
+    @GetMapping("/home") // Define your endpoint path
+    public String yourPage() {
+        // Add any necessary model attributes if needed
+        return "home";
+    }
+}
