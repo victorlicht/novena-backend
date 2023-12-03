651291405f0bf6f54df771cd3d36e2df793d31b9
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-12-03 20:16:33 +0100

    Add CRUD methods to PatientServiceImpl

--- src/main/java/com/victorlicht/novenabackend/services/PatientService.java
+++ src/main/java/com/victorlicht/novenabackend/services/PatientService.java
@@ -1,4 +1,15 @@
 package com.victorlicht.novenabackend.services;
 
+import com.victorlicht.novenabackend.dtos.PatientDto;
+import com.victorlicht.novenabackend.models.Patient;
+
+import java.util.List;
+
 public interface PatientService {
+
+    PatientDto createPatientAccount(PatientDto patientDto);
+    PatientDto updatePatientAccount(PatientDto patientDto, Patient patient);
+    void deletePatientAccount(PatientDto patientDto);
+    List<PatientDto> findAllPatients();
+
 }
--- src/main/java/com/victorlicht/novenabackend/services/PatientServiceImpl.java
+++ src/main/java/com/victorlicht/novenabackend/services/PatientServiceImpl.java
@@ -1,4 +1,43 @@
 package com.victorlicht.novenabackend.services;
 
-public class PatientServiceImpl {
+import com.victorlicht.novenabackend.dtos.PatientDto;
+import com.victorlicht.novenabackend.mapper.PatientMapper;
+import com.victorlicht.novenabackend.models.Patient;
+import com.victorlicht.novenabackend.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements PatientService{
+
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public PatientServiceImpl(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Override
+    public PatientDto createPatientAccount(PatientDto patientDto) {
+        return PatientMapper.toDto(patientRepository.save(PatientMapper.toEntity(patientDto)));
+    }
+
+    @Override
+    public PatientDto updatePatientAccount(PatientDto patientDto, Patient patient) {
+        PatientMapper.updateEntityFromDto(patientDto, patient);
+        return PatientMapper.toDto(patientRepository.save(patient));
+    }
+
+    @Override
+    public void deletePatientAccount(PatientDto patientDto) {
+        patientRepository.delete(PatientMapper.toEntity(patientDto));
+    }
+
+    @Override
+    public List<PatientDto> findAllPatients() {
+        return PatientMapper.findAllPatientsToDto(patientRepository.findAll());
+    }
+
 }
