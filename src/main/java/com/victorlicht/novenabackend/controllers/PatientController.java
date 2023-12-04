5f60da32675af4eefd8690a9bc56f64a40be56d1
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-12-04 18:08:08 +0100

    Add createPatientAccount method in controller

--- src/main/java/com/victorlicht/novenabackend/controllers/PatientController.java
+++ src/main/java/com/victorlicht/novenabackend/controllers/PatientController.java
@@ -1,12 +1,15 @@
 package com.victorlicht.novenabackend.controllers;
 
+import com.victorlicht.novenabackend.dtos.PatientDto;
+import com.victorlicht.novenabackend.mapper.PatientMapper;
+import com.victorlicht.novenabackend.models.Patient;
 import com.victorlicht.novenabackend.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/v1/patients")
@@ -32,5 +35,24 @@ public class PatientController {
                     .body("Server Error, Try Later" + e.getMessage());
         }
     }
+
+    @PostMapping("/admin/create")
+    public ResponseEntity<?> createPatientAccount(@Validated @RequestBody PatientDto patientDto) {
+        try {
+            PatientDto createdPatient = patientService.createPatientAccount(patientDto);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(createdPatient);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Invalid input: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to create patient account: " + e.getMessage());
+        }
+    }
+
 
 }
