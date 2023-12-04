5152503f8837e8737e88c3a8f1eed48a02e6daeb
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-12-04 18:07:34 +0100

    Add findAllPatients method in controller

--- src/main/java/com/victorlicht/novenabackend/controllers/PatientController.java
+++ src/main/java/com/victorlicht/novenabackend/controllers/PatientController.java
@@ -1,8 +1,36 @@
 package com.victorlicht.novenabackend.controllers;
 
+import com.victorlicht.novenabackend.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1/patients")
 public class PatientController {
+    //TODO: Patient(Admin Control) Create, List & Filter, Update, Search, Delete
+
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllPatients() {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(patientService.findAllPatients());
+        }catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Server Error, Try Later" + e.getMessage());
+        }
+    }
 
 }
